132d2088f56c2b67fea4de809dcf595bae702229
unknown <thalesjoazeiro@gmail.com>
2023-10-09 10:05:04 -0300

    Exercício 02

---
+++ 02-valorJantar.js
@@ -0,0 +1,7 @@
+let gastos = 80;
+let garcom = 0;
+
+console.log('Valor do jantar: R$ ' + gastos + ',00');
+garcom = ((gastos*10)/100);
+console.log('Taxa do garçom: R$ ' + garcom + ',00');
+console.log('Total a pagar: R$ ' + (gastos + garcom) + ',00');
